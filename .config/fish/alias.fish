8debdfd0bef9d49deaabaf1d9d3abefbc0e35c31
nkimoto <kimoppy126@gmail.com>
2018-12-12 12:32:21 +0900

    Add alias config file

---
+++ .config/fish/alias.fish
@@ -0,0 +1,26 @@
+alias g 'git'
+
+alias ga 'git add'
+alias gaa 'git add --all'
+
+alias gb 'git branch'
+alias gba 'git branch -a'
+alias gbd 'git branch -d'
+
+alias gc 'git commit -v'
+alias gc! 'git commit -v --amend'
+
+alias vi '/usr/bin/vim'
+alias rm 'rmtrash'
+alias ll 'ls --human-readable -l'
+
+alias pull 'git pull'
+alias push 'git push'
+alias gs 'git status'
+
+alias gd 'git diff'
+alias gl "git log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'"
+
+alias rm 'rmtrash'
+
+
